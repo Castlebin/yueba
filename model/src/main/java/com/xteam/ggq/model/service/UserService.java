f63a6701bdeb8694450d5dd7d762f3a5ed7b6ac0
gaoyuxin <Castlebin@gmail.com>
2016-04-10 00:31:01 +0800

    no exception

--- model/src/main/java/com/xteam/ggq/model/service/UserService.java
+++ model/src/main/java/com/xteam/ggq/model/service/UserService.java
@@ -4,7 +4,6 @@ import com.xteam.ggq.model.bo.User;
 import com.xteam.ggq.model.bo.UserTag;
 import com.xteam.ggq.model.dao.UserRepository;
 import com.xteam.ggq.model.dao.UserTagRepository;
-import com.xteam.ggq.model.exception.BizException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -23,10 +22,6 @@ public class UserService {
 
     public User findUser(String username) {
         User user = userRepository.findByUsername(username);
-
-        if (user == null) {
-            throw new BizException("没有找到相应的用户！");
-        }
 
         Set<UserTag> userTags = userTagRepository.findByUsername(username);
         Set<String> tags = userTags.stream().map(UserTag::getTagName).collect(Collectors.toSet());
