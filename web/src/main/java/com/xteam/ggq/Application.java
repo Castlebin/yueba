5e8ce2cea4c8b388ee032bdef79529fa59c66ca9
Castlebin <Castlebin@gmail.com>
2016-04-09 12:29:43 +0800

    add file upload controller

--- web/src/main/java/com/xteam/ggq/Application.java
+++ web/src/main/java/com/xteam/ggq/Application.java
@@ -1,23 +1,35 @@
 package com.xteam.ggq;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.web.SpringBootServletInitializer;
-
+import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
+
+import java.io.File;
 
 @SpringBootApplication
 @EnableScheduling
 public class Application extends SpringBootServletInitializer {
+
+    public static final String ROOT = "/files";
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
 
     @Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
         return application.sources(Application.class);
     }
 
-    public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+    @Bean
+    CommandLineRunner init() {
+        return (String[] args) -> {
+            new File(ROOT).mkdirs();
+        };
     }
 
 }
