9345f5170e335753d8265acc624c05050eba80b9
guoshengxing <guoshengxing@renrendai.com>
2016-04-09 22:19:31 +0800

    优化显示已参加未完成活动数的展示

--- web/src/main/resources/public/dist/controllers/usercenter.js
+++ web/src/main/resources/public/dist/controllers/usercenter.js
@@ -72,28 +72,4 @@ yuebaApp.controller('UserCenterController', ['$scope', '$http', '$q', 'UserServi
     };
 
     $scope.list();
-
-    $scope.currentActivityNum = 0;
-
-    $scope.getCurrentActivityNum = function () {
-        $http({
-            method: 'GET',
-            url: '/api/activity/curractnum'
-        }).then(
-            function (response) {
-                var serverResponse = response.data;
-                if (angular.isObject(serverResponse) && serverResponse.status == 0) {
-                    $scope.currentActivityNum = serverResponse.data;
-                }
-                else {
-                    $.alert('得到用户当前活动数量失败了');
-                }
-            },
-            function (response) {
-                $.alert('得到用户当前活动数量失败了');
-            }
-        );
-    }
-
-    $scope.getCurrentActivityNum();
 }]);
